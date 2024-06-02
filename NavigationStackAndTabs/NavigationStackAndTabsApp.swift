dbd5e8c73b478b6774aa6f135401fcdb41294e23
hooneun <kimhoon0316@gmail.com>
2024-06-02 15:43:03 +0900

    init

---
+++ NavigationStackAndTabs/NavigationStackAndTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAndTabsApp.swift
+//  NavigationStackAndTabs
+//
+//  Created by Hoon on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAndTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
