fc494274561ff00ac9a8018b534e6e8a5e6bdca7
hooneun <kimhoon0316@gmail.com>
2024-06-02 17:06:06 +0900

    .

--- NavigationStackAndTabs/NavigationStackAndTabsApp.swift
+++ NavigationStackAndTabs/NavigationStackAndTabsApp.swift
@@ -12,6 +12,7 @@ struct NavigationStackAndTabsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(NavigationPathFinder.shared)
         }
     }
 }
